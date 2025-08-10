a0fa6cfa2f95223a23b5b522b4fc40a61ee01273
ElectonAmpere <82173777+ElectronAmpere@users.noreply.github.com>
2025-08-10 12:56:10 +0530

    Updated implementation for absurd value check

--- src/stats.c
+++ src/stats.c
@@ -1,24 +1,62 @@
 #include "stats.h"
-#include <stddef.h>
 #include <float.h>
+#include <math.h>
+#include <stddef.h>
 
-struct Stats compute_statistics(const float* numbers, int count) {
-    struct Stats statistics;
-    float sum = 0.0f;
-    statistics.average = 0.0f;
-    statistics.min = FLT_MAX;
-    statistics.max = -FLT_MAX;
-
-    for (int index = 0; index < count; index++) {
-        if (numbers[index] < statistics.min) {
-            statistics.min = numbers[index];
-        }
-        if (numbers[index] > statistics.max) {
-            statistics.max = numbers[index];
-        }
-        sum += numbers[index];
+int checkValueAbsurdity(const float *numbers, int count) {
+  for (int index = 0; index < count; index++) {
+    if (numbers[index] < 0 || numbers[index] > 1e5) {
+      return 1;
     }
+  }
+  return 0;
+}
 
-    statistics.average = (sum / count);
+struct Stats compute_statistics(const float *numbers, int count) {
+  struct Stats statistics;
+  float sum = 0.0f;
+  statistics.average = 0.0f;
+  statistics.min = FLT_MAX;
+  statistics.max = -FLT_MAX;
+  int validCount = 0;
+
+  if (count <= 0) {
+    statistics.min = NAN;
+    statistics.max = NAN;
+    statistics.average = NAN;
     return statistics;
+  }
+
+  if (checkValueAbsurdity(numbers, count)) {
+    statistics.min = NAN;
+    statistics.max = NAN;
+    statistics.average = NAN;
+    return statistics;
+  }
+
+  for (int index = 0; index < count; index++) {
+
+    if (isnan(numbers[index])) {
+      continue;
+    }
+    validCount++;
+
+    if (numbers[index] < statistics.min) {
+      statistics.min = numbers[index];
+    }
+    if (numbers[index] > statistics.max) {
+      statistics.max = numbers[index];
+    }
+    sum += numbers[index];
+  }
+
+  if (validCount != 0) {
+    statistics.average = (sum / validCount);
+  } else {
+    statistics.min = NAN;
+    statistics.max = NAN;
+    statistics.average = NAN;
+  }
+
+  return statistics;
 }
--- test/stats-test.cpp
+++ test/stats-test.cpp
@@ -4,15 +4,45 @@
 #include <math.h>
 
 TEST(Statistics, ReportsAverageMinMax) {
-    float values[] = {1.5, 8.9, 3.2, 4.5};
-    auto computedStats = compute_statistics(values, 4);
-    float epsilon = 0.001;
-    EXPECT_LT(fabsf(computedStats.average - 4.525), epsilon);
-    EXPECT_LT(fabsf(computedStats.max - 8.9), epsilon);
-    EXPECT_LT(fabsf(computedStats.min - 1.5), epsilon);
+  float values[] = {1.5, 8.9, 3.2, 4.5};
+  auto computedStats = compute_statistics(values, 4);
+  float epsilon = 0.001;
+  EXPECT_LT(fabsf(computedStats.average - 4.525), epsilon);
+  EXPECT_LT(fabsf(computedStats.max - 8.9), epsilon);
+  EXPECT_LT(fabsf(computedStats.min - 1.5), epsilon);
 }
 
 TEST(Statistics, AverageNaNForEmpty) {
-    float values[1] = {};
-    auto computedStats = compute_statistics(values, 0);
+  float values[1] = {};
+  auto computedStats = compute_statistics(values, 0);
+  // No way to check for NaN other than isnan() function
+  EXPECT_TRUE(isnan(computedStats.average));
+  EXPECT_TRUE(isnan(computedStats.min));
+  EXPECT_TRUE(isnan(computedStats.max));
+}
+
+TEST(Statistics, AverageIgnoreNaN) {
+  float values[] = {1.5, 8.9, NAN, 4.5};
+  auto computedStats = compute_statistics(values, 4);
+  float epsilon = 0.001;
+  EXPECT_LT(fabsf(computedStats.average - 4.967), epsilon);
+  EXPECT_LT(fabsf(computedStats.max - 8.9), epsilon);
+  EXPECT_LT(fabsf(computedStats.min - 1.5), epsilon);
+}
+
+TEST(Statistics, AverageOfAllNaN) {
+  float values[] = {NAN, NAN, NAN};
+  auto computedStats = compute_statistics(values, 3);
+  EXPECT_TRUE(isnan(computedStats.average));
+  EXPECT_TRUE(isnan(computedStats.min));
+  EXPECT_TRUE(isnan(computedStats.max));
+}
+
+TEST(Statistics, AverageNaNForAbsurdValues) {
+  float values[] = {1e10, -1, 8.9, 4.5, 1000};
+  auto computedStats = compute_statistics(values, 5);
+  float epsilon = 0.001;
+  EXPECT_TRUE(isnan(computedStats.average));
+  EXPECT_TRUE(isnan(computedStats.min));
+  EXPECT_TRUE(isnan(computedStats.max));
 }
